b5e0a2b7f4fa2d7f72d7a08a16dc56e257a71a94
Kassimbot <115211119+Kassimbot@users.noreply.github.com>
2023-03-22 02:06:28 +0530

    Update thumbnails.py

--- ShizukaXMusic/utils/thumbnails.py
+++ ShizukaXMusic/utils/thumbnails.py
@@ -12,10 +12,6 @@ import numpy as np
 from config import YOUTUBE_IMG_URL
 
 
-def make_col():
-    return (random.randint(0,255),random.randint(0,255),random.randint(0,255))
-
-
 def changeImageSize(maxWidth, maxHeight, image):
     widthRatio = maxWidth / image.size[0]
     heightRatio = maxHeight / image.size[1]
